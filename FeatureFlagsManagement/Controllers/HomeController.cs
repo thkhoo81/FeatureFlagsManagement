c0ad8f84355258f0453ff01fbe659bb9ce0b217d
Khoo TH <TeikHeong.Khoo@kerrylogistics.com>
2021-01-11 19:18:42 +0800

    Add project files.

---
+++ FeatureFlagsManagement/Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using FeatureFlagsManagement.Models;
+using Microsoft.FeatureManagement;
+using Microsoft.FeatureManagement.Mvc;
+
+namespace FeatureFlagsManagement.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        //private readonly IFeatureManager _featureManager;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        //[FeatureGate(MyFeatureManagementFlags.NewFeature1)]
+        [Route("[controller]/[action]")]
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        
+        [Route("[controller]/[action]")]
+        [FeatureGate(MyFeatureManagementFlags.NewFeature3)]
+        public IActionResult NewFeature3()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ FeatureFlagsManagement/Models/Enum.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FeatureFlagsManagement.Models
+{
+    public enum MyFeatureManagementFlags
+    {
+
+
+        NewFeature1,
+        NewFeature2,
+        NewFeature3
+
+    }
+}
